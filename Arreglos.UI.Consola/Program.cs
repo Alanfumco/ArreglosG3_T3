c412e28ecd64feab1fb3572ce665cdeae33b1d0f
Alanfumco <24030053@itesa.edu.mx>
2026-09-17 08:18:23 -0600

    Version_02: Metodo llenar arreglo

--- Arreglos.UI.Consola/Program.cs
+++ Arreglos.UI.Consola/Program.cs
@@ -1,1 +1,16 @@
-﻿    Console.WriteLine("Hello, World!");
+﻿using ArraglosLogica;
+
+Console.WriteLine("Arreglos "),
+
+    MiArreglo oMyArreglo = new(100);
+
+oMyArreglo.Llenar(1, 100);
+
+Console.WriteLine("\nArreglo desordenado: ");
+Console.WriteLine(oMyArreglo);
+
+Console.WriteLine("\nArreglo ordenado: ");
+oMyArreglo.Ordenar();
+Console.WriteLine(oMyArreglo);
+
+Console.ReadKey();
--- ArreglosLogica/MiArreglo.cs
+++ ArreglosLogica/MiArreglo.cs
@@ -6,7 +6,67 @@ namespace ArreglosLogica
 {
     public class MiArreglo
     {
+        //Propiedades
+        public int N { get; }
+        public bool EstaLleno => _tope == N;
+        public bool EstaVacio => _tope == 0;
 
-       
+        //Metodos
+        public void Llenar(int minimo, int maximo)
+        {
+            Random random = new Random();
+            for (int i=0; i < N; i++)
+            {
+                arreglo[i] = random.Next(minimo, maximo);
+
+            }
+        }
+
+        //Metodo ordenar (Burbuja)
+        public void Ordenar()
+        {
+            for (int i = 0; i < _tope - 1; i++)
+            {
+                for (int j = i + 1; j < _tope; j++)
+                {
+                    if(_arreglo[i] > _arreglo[j])
+                    {
+                        Cambiar(ref _arreglo[i], ref _arreglo[j]);
+                    }
+                }
+            }
+        }
+
+        //Metodo Cambiar
+        public void Cambiar(ref int a, ref int b)
+        {
+            int auxiliar = a;
+            a = b;
+            b = auxiliar;
+
+        }
+
+        //Metodo ToString
+        public override string ToString()
+        {
+            if (EstaVacio)
+            {
+                Console.WriteLine("El arreglo está vacío.");
+
+            }
+            int contador = 0;
+            contador++;
+            for (int i=0; i<_tope; i++)
+            {
+                salida += $"{arreglo[i]}\t";
+                if(contador > 9)
+                {
+                    contador = 0;
+                    salida += "\n";
+                }
+            }
+            return salida;
+
+        }
     }
 }
